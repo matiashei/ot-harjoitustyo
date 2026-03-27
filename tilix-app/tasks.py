5a339dd50236013232d1480b55fa1522fc23df82
Matias Heino <matias.heino@hotmail.com>
2026-03-27 16:37:58 +0200

    Add week 3: 5

---
+++ tilix-app/tasks.py
@@ -0,0 +1,15 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py")
+
+@task
+def test(ctx):
+    ctx.run("pytest")
+
+@task(name="coverage-report")
+def coverage_report(ctx):
+    ctx.run("coverage run -m pytest")
+    ctx.run("coverage report -m")
+    ctx.run("coverage html")
