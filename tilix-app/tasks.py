bea9b995534bf25dbb67e4a87acac8978d105d13
Matias Heino <matias.heino@hotmail.com>
2026-03-31 19:24:49 +0300

    Add invoke task for pylint

--- tilix-app/tasks.py
+++ tilix-app/tasks.py
@@ -13,3 +13,7 @@ def coverage_report(ctx):
     ctx.run("coverage run -m pytest")
     ctx.run("coverage report -m")
     ctx.run("coverage html")
+
+@task(name="lint")
+def lint(ctx):
+    ctx.run("pylint src")
